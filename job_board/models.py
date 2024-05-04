4a6367b7e971cd44b8de4db99d57a810ece0834d
Pylos7 <nestor1ingles@gmail.com>
2024-05-04 13:45:56 -0400

    create model add fields

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+# model => python class
+# model represents a table in the db
+# attributes are the fields
+
+class JobPosting(models.Model):
+	title = models.CharField(max_length=50)
+	description = models.TextField()
+	company = models.CharField(max_length=100)
+	salary = models.IntegerField()
+	is_active = models.BooleanField(default=False)
+
+# makemigrations
+## -> creates intructions telling django how the db have changed
+
+# migrate
+# -> Applies the above changes
+
+
+# CRUD Operations
+# Create
+# Read
+# Update
+# Delete
+
+
+# model manager -> objects
+# JobPosting.objects.all()
+# JobPosting.objects.get(id=1)
+
