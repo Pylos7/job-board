8776cb09f8a83fc7c191862e8e31929c16cc04b2
Pylos7 <nestor1ingles@gmail.com>
2024-05-04 18:50:57 -0400

    add and use get_odject_or_404 function

--- job_board/views.py
+++ job_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import JobPosting
 
@@ -9,3 +9,11 @@ def index(request):
 		'job_postings': active_postings
 	}
 	return render(request, 'job_board/index.html', context)
+
+
+def job_detail(request, pk):
+	job_posting = get_object_or_404(JobPosting, pk=pk, is_active=True)
+	context = {
+		'posting': job_posting
+	}
+	return render(request, 'job_board/detail.html', context)
